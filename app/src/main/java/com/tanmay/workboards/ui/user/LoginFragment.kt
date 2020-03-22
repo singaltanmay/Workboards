cd6a7e9569aa3b5c245020d8d1dd0b23645a6888
Tanmay Singal <tanmaysingal2013@gmail.com>
2020-03-22 14:39:16 +0530

    Finished building login fragment

--- app/src/main/java/com/tanmay/workboards/ui/user/LoginFragment.kt
+++ app/src/main/java/com/tanmay/workboards/ui/user/LoginFragment.kt
@@ -6,10 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.tanmay.workboards.R
+import kotlinx.android.synthetic.main.fragment_login.*
 import kotlinx.android.synthetic.main.fragment_login.frag_user_login_cancel_button as cancelButton
+import kotlinx.android.synthetic.main.fragment_login.frag_user_login_next_button as loginButton
+import kotlinx.android.synthetic.main.fragment_login.frag_user_login_sing_up_button as signUpButton
 
 class LoginFragment : Fragment() {
 
@@ -28,30 +32,39 @@ class LoginFragment : Fragment() {
         Thread { setupListeners() }.start()
     }
 
-
     private fun setupListeners() {
         cancelButton.setOnClickListener {
             findNavController().popBackStack()
         }
-//            listener?.onCancel()
-//        }
-//        loginButton.setOnClickListener {
+        loginButton.setOnClickListener {
 //            listener?.onLogin(-1)
-//        }
-//        signUpButton.setOnClickListener {
+        }
+        signUpButton.setOnClickListener {
 //            findNavController().navigate(R.id.action_userLoginFragment_to_userCreationFragment)
-//        }
+        }
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(LoginViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+
+        viewModel.email.observe(viewLifecycleOwner, Observer {
+            frag_user_login_email_id_edit_text.text = it
+        })
+
+        viewModel.password.observe(viewLifecycleOwner, Observer {
+            frag_user_login_password_edit_text.text = it
+        })
+
     }
 
     override fun onPause() {
         super.onPause()
         (activity as AppCompatActivity).supportActionBar?.show()
+    }
+
+    interface OnUserLoginListener {
+        fun onLogin(userId: Long)
     }
 
 }
--- app/src/main/java/com/tanmay/workboards/ui/user/LoginViewModel.kt
+++ app/src/main/java/com/tanmay/workboards/ui/user/LoginViewModel.kt
@@ -1,7 +1,16 @@
 package com.tanmay.workboards.ui.user
 
+import android.text.Editable
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class LoginViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private val _email = MutableLiveData<Editable>()
+    private val _password = MutableLiveData<Editable>()
+
+    val email: LiveData<Editable> = _email
+    val password: LiveData<Editable> = _password
+
 }
